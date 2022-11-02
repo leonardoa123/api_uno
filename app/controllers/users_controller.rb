aaf290c9709edd9712c1da0643c64f60b2d26bb8
Leonardo <leonardoa123@gmail.com>
2022-11-01 23:53:59 -0300

    add routes and controller auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,23 @@ class UsersController < ApplicationController
 
         if @user.valid?
             token =encode_token( { user_id: @user.id })
-
             render json: { token: token}, status: :ok
         else
             render json: { error: "Unable to log in with provided credentials"}, status: :unprocessable_entity
         end 
     end
 
+    def auth
+        @user = User.find_by(email: user_params[:email])
+
+        if @user && @user.authenticate(user_params[:password])
+            token =encode_token( { user_id: @user.id })
+            render json: { token: token}, status: :ok
+        else
+            render json: { error: "Unable to log in with provided credentials"}, status: :unprocessable_entity
+        end
+
+    end
 
     private
 
